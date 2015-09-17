13ea942daf5ce24c509130ad6519a3f19f389893
Kenneth Verheggen <kenneth.verheggen@gmail.com>
2015-09-17 12:26:55 +0200

    Renamed DenovoGUI to DeNovoGUI

---
+++ Pladipus-denovo/src/main/java/com/compomics/pladipus/denovo/processsteps/DeNovoGUISetupStep.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.compomics.pladipus.denovo.processsteps;
+
+import com.compomics.pladipus.core.control.util.PladipusFileDownloadingService;
+import com.compomics.pladipus.core.model.processing.ProcessingStep;
+import java.io.File;
+import org.apache.commons.io.FileUtils;
+
+/**
+ *
+ * @author Kenneth Verheggen
+ */
+public class DeNovoGUISetupStep extends ProcessingStep {
+
+    /**
+     * the temp folder for the entire processing
+     */
+    private final File tempResources;
+
+    public DeNovoGUISetupStep() {
+        tempResources = new File(System.getProperty("user.home") + "/.compomics/pladipus/temp/SearchGUI/resources");
+    }
+
+    @Override
+    public boolean doAction() throws Exception {
+        System.out.println("Running " + this.getClass().getName());
+        if (tempResources.exists()) {
+            for (File aFile : tempResources.listFiles()) {
+                if (aFile.exists()) {
+                    if (aFile.isFile()) {
+                        aFile.delete();
+                    } else {
+                        FileUtils.deleteDirectory(aFile);
+                    }
+                }
+            }
+        } else {
+            tempResources.mkdirs();
+        }
+        initialiseInputFiles();
+        return true;
+    }
+
+    private void initialiseInputFiles() throws Exception {
+        //original
+        String inputPath = parameters.get("spectrum_files");
+        String paramPath = parameters.get("id_params");
+
+        if (inputPath.toLowerCase().endsWith(".mgf")) {
+            parameters.put("spectrum_files", PladipusFileDownloadingService.downloadFile(inputPath, tempResources).getAbsolutePath());
+        } else {
+            parameters.put("spectrum_files", PladipusFileDownloadingService.downloadFolder(inputPath, tempResources).getAbsolutePath());
+        }
+
+        parameters.put("id_params", PladipusFileDownloadingService.downloadFile(paramPath, tempResources).getAbsolutePath());
+
+        //output
+        File outputFolder = new File(parameters.get("output_folder") + "/" + parameters.get("title"));
+        outputFolder.mkdirs();
+        parameters.put("output_folder", outputFolder.getAbsolutePath());
+    }
+
+    @Override
+    public String getDescription() {
+        return "Initialisation of the search process";
+    }
+
+}
---
+++ Pladipus-denovo/src/main/java/com/compomics/pladipus/denovo/processsteps/DeNovoGUIStep.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.compomics.pladipus.denovo.processsteps;
+
+import com.compomics.pladipus.core.control.engine.ProcessingEngine;
+import com.compomics.pladipus.core.control.util.JarLookupService;
+import com.compomics.pladipus.core.control.util.PladipusFileDownloadingService;
+import com.compomics.pladipus.core.control.util.ZipUtils;
+import com.compomics.pladipus.core.model.enums.AllowedDenovoGUIParams;
+import com.compomics.pladipus.core.model.processing.ProcessingStep;
+import com.compomics.util.experiment.identification.identification_parameters.SearchParameters;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
+
+/**
+ *
+ * @author Kenneth Verheggen
+ */
+public class DeNovoGUIStep extends ProcessingStep {
+
+    private static final Logger LOGGER = Logger.getLogger(DeNovoGUIStep.class);
+    private static final File temp_deNovoGUI_output = new File(System.getProperty("user.home") + "/.compomics/pladipus/temp/DeNovoGUI/result");
+
+    public DeNovoGUIStep() {
+
+    }
+
+    private List<String> constructArguments() throws IOException {
+        File deNovoGUIJar = getJar();
+        ArrayList<String> cmdArgs = new ArrayList<>();
+        cmdArgs.add("java");
+        cmdArgs.add("-cp");
+        cmdArgs.add(deNovoGUIJar.getAbsolutePath());
+        cmdArgs.add("com.compomics.denovogui.cmd.DeNovoCLI");
+        for (AllowedDenovoGUIParams aParameter : AllowedDenovoGUIParams.values()) {
+            if (parameters.containsKey(aParameter.getId())) {
+                cmdArgs.add("-" + aParameter.getId());
+                cmdArgs.add(parameters.get(aParameter.getId()));
+            } else if (aParameter.isMandatory()) {
+                throw new IllegalArgumentException("Missing mandatory parameter : " + aParameter.id);
+            }
+        }
+        return cmdArgs;
+    }
+
+    @Override
+    public boolean doAction() throws Exception, Exception {
+        LOGGER.info("Running " + this.getClass().getName());
+        File parameterFile = new File(parameters.get("id_params"));
+        LOGGER.info("Updating parameters...");
+        SearchParameters identificationParameters = SearchParameters.getIdentificationParameters(parameterFile);
+        //fix the location
+        identificationParameters.setParametersFile(parameterFile);
+        SearchParameters.saveIdentificationParameters(identificationParameters, parameterFile);
+        if (temp_deNovoGUI_output.exists()) {
+            temp_deNovoGUI_output.delete();
+        }
+        temp_deNovoGUI_output.mkdirs();
+
+        LOGGER.info("Starting searchGUI...");
+        //use this variable if you'd run following this classs
+        File real_outputFolder = new File(parameters.get("output_folder"));
+        parameters.put("output_folder", temp_deNovoGUI_output.getAbsolutePath());
+        ProcessingEngine.startProcess(getJar(), constructArguments());
+        //storing intermediate results
+        LOGGER.info("Storing results in " + real_outputFolder);
+        FileUtils.copyDirectory(temp_deNovoGUI_output, real_outputFolder);
+        //in case of future peptideShaker searches : 
+        parameters.put("identification_files", temp_deNovoGUI_output.getAbsolutePath());
+        return true;
+    }
+
+    public File getJar() throws IOException {
+        //check if this is possible in another way...
+        File toolFolder = new File(System.getProperties().getProperty("user.home") + "/.compomics/pladipus/tools");
+        toolFolder.mkdirs();
+        //check if searchGUI already exists?
+        File temp = new File(toolFolder, "DeNovoGUI");
+        if (!temp.exists()) {
+            File searchGUIFile = PladipusFileDownloadingService.downloadFile(parameters.get("DeNovoGUI"), toolFolder);
+            if (searchGUIFile.getName().endsWith(".zip")) {
+                ZipUtils.unzipArchive(searchGUIFile, temp);
+            }
+        }
+        return JarLookupService.lookupFile("DeNovoGUI-.*.jar", temp);
+    }
+
+    public boolean aVersionExistsLocal() {
+        //TODO insert installer code here in case searchGUI was not included????
+        return true;
+    }
+
+    @Override
+    public String getDescription() {
+        return "Running DenovoGUI";
+    }
+
+}
