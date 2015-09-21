376e8aedb3d2eb9c7ed320be4fdaf23d7e8be344
Kenneth Verheggen <kenneth.verheggen@gmail.com>
2015-09-21 10:50:54 +0200

    Added the option to unzip mgf files (very handy)

--- Pladipus-search/src/main/java/com/compomics/pladipus/search/processsteps/SearchSetupStep.java
+++ Pladipus-search/src/main/java/com/compomics/pladipus/search/processsteps/SearchSetupStep.java
@@ -1,9 +1,11 @@
 package com.compomics.pladipus.search.processsteps;
 
 import com.compomics.pladipus.core.control.util.PladipusFileDownloadingService;
+import com.compomics.pladipus.core.control.util.ZipUtils;
 import com.compomics.pladipus.core.model.processing.ProcessingStep;
 import java.io.File;
 import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
 
 /**
  *
@@ -15,9 +17,11 @@ public class SearchSetupStep extends ProcessingStep {
      * the temp folder for the entire processing
      */
     private final File tempResources;
+    private static final Logger LOGGER = Logger.getLogger(SearchSetupStep.class);
 
     public SearchSetupStep() {
         tempResources = new File(System.getProperty("user.home") + "/.compomics/pladipus/temp/SearchGUI/resources");
+        tempResources.getParentFile().mkdirs();
     }
 
     @Override
@@ -46,19 +50,26 @@ public class SearchSetupStep extends ProcessingStep {
         String paramPath = parameters.get("id_params");
         String fastaPath = parameters.get("fasta_file");
 
-        if (inputPath.toLowerCase().endsWith(".mgf")) {
-            parameters.put("spectrum_files", PladipusFileDownloadingService.downloadFile(inputPath, tempResources).getAbsolutePath());
+        if (inputPath.toLowerCase().endsWith(".mgf") || inputPath.toLowerCase().endsWith(".mgf.zip")) {
+            String inputFile = PladipusFileDownloadingService.downloadFile(inputPath, tempResources).getAbsolutePath();
+            if (inputPath.toLowerCase().endsWith(".zip")) {
+                LOGGER.info("Unzipping input");
+                ZipUtils.unzipArchive(new File(inputFile), tempResources);
+                LOGGER.info("Done unzipping...");
+            }
+            parameters.put("spectrum_files", inputFile.replace(".zip", ""));
         } else {
             parameters.put("spectrum_files", PladipusFileDownloadingService.downloadFolder(inputPath, tempResources).getAbsolutePath());
         }
-
+        LOGGER.info("Got input files " + parameters.get("spectrum_files"));
         parameters.put("id_params", PladipusFileDownloadingService.downloadFile(paramPath, tempResources).getAbsolutePath());
-
+        LOGGER.info("Got identification parameters " + parameters.get("id_params"));
         parameters.put("fasta_file", PladipusFileDownloadingService.downloadFile(fastaPath, tempResources, "temp.fasta").getAbsolutePath());
-
+        LOGGER.info("Got fasta file " + parameters.get("fasta_file"));
         //output
         File outputFolder = new File(parameters.get("output_folder"));
         outputFolder.mkdirs();
+        LOGGER.info("Got output folder " + parameters.get("output_folder"));
         parameters.put("output_folder", outputFolder.getAbsolutePath());
     }
 
