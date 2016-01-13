eb20a3cc7bc06d38ebb9cb7f080c21858c3a3303
compomics <compomics@157.193.143.131>
2016-01-13 15:57:15 +0100

    included example files in the installation

--- Pladipus-moff/src/main/java/com/compomics/pladipus/moff/logic/MoffInstaller.java
+++ Pladipus-moff/src/main/java/com/compomics/pladipus/moff/logic/MoffInstaller.java
@@ -4,11 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.Enumeration;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
 import org.apache.commons.io.FileUtils;
-import sun.misc.Launcher;
 
 /**
  *
@@ -20,6 +16,10 @@ public class MoffInstaller {
         File destFile = new File(System.getProperty("user.home") + "/pladipus/tools/moff");
         URL resource = MoffInstaller.class.getClassLoader().getResource("moFF");
         File resourceFolder = new File(resource.toURI());
+        FileUtils.copyDirectory(resourceFolder, destFile);
+        destFile = new File(System.getProperty("user.home") + "/pladipus/tools/moff/example");
+        resource = MoffInstaller.class.getClassLoader().getResource("example");
+        resourceFolder = new File(resource.toURI());
         FileUtils.copyDirectory(resourceFolder, destFile);
     }
 }
