ecadda6f4b6afb5a309271302e5ca59a0be0fc30
compomics <compomics@157.193.143.131>
2017-05-17 11:36:56 +0200

    Fixed bug in pride asap file mgf extraction (nested zip file)

--- Pladipus-search/src/main/java/com/compomics/pladipus/search/util/PrideAsapOutputExtractor.java
+++ Pladipus-search/src/main/java/com/compomics/pladipus/search/util/PrideAsapOutputExtractor.java
@@ -8,6 +8,7 @@ package com.compomics.pladipus.search.util;
 import static com.compomics.pladipus.core.control.util.ZipUtils.zipFile;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
@@ -24,20 +25,20 @@ import org.apache.log4j.Logger;
  * @author Kenneth Verheggen <kenneth.verheggen@gmail.com>
  */
 public class PrideAsapOutputExtractor {
-
+    
     private static final Logger LOGGER = Logger.getLogger(PrideAsapOutputExtractor.class);
-
+    
     private final File prideasapZip;
     private final File outputFolder;
     private File parameterFile;
     private File mgfFile;
-
+    
     public PrideAsapOutputExtractor(File prideasapZip, File outputFolder) {
         this.prideasapZip = prideasapZip;
         this.outputFolder = outputFolder;
         extract();
     }
-
+    
     private void extract() {
         try (ZipFile input = new ZipFile(prideasapZip)) {
             Enumeration<? extends ZipEntry> entries = input.entries();
@@ -52,18 +53,18 @@ public class PrideAsapOutputExtractor {
                 }
             }
             if (paramEntry != null && mgfEntry != null) {
-
+                
                 extractParameters(input, paramEntry);
                 extractMGF(input, mgfEntry);
             }
         } catch (IOException ex) {
             LOGGER.error(ex);
         }
-
+        
     }
-
+    
     private void extractParameters(ZipFile input, ZipEntry paramEntry) throws IOException {
-        File outputFile = new File(outputFolder, input.getName().replace(".pzip", ".par"));
+        File outputFile = new File(outputFolder, new File(input.getName().replace(".pzip", ".par")).getName());
         try (BufferedReader stream = new BufferedReader(new InputStreamReader(input.getInputStream(paramEntry)));
                 FileWriter out = new FileWriter(outputFile);) {
             String line;
@@ -73,35 +74,34 @@ public class PrideAsapOutputExtractor {
             out.flush();
             parameterFile = outputFile;
         }
-
+        
     }
-
+    
     private void extractMGF(ZipFile input, ZipEntry mgfEntry) throws IOException {
         try (ZipInputStream innerZip = new ZipInputStream(input.getInputStream(mgfEntry))) {
-            File outputFile = new File(outputFolder, input.getName().replace(".pzip", ".mgf"));
+            File outputFile = new File(outputFolder, new File(input.getName().replace(".pzip", ".mgf")).getName());
             ZipEntry mgfZipEntry = null;
+            byte[] buffer = new byte[2048];
             while ((mgfZipEntry = innerZip.getNextEntry()) != null) {
                 if (mgfZipEntry.getName().endsWith(".mgf")) {
-                    try (BufferedReader stream = new BufferedReader(new InputStreamReader(input.getInputStream(mgfZipEntry)));
-                            FileWriter out = new FileWriter(outputFile);) {
-                        String line;
-                        while ((line = stream.readLine()) != null) {
-                            out.append(line + System.lineSeparator()).flush();
+                    try (FileOutputStream output = new FileOutputStream(outputFile)) {
+                        int len;
+                        while ((len = innerZip.read(buffer)) > 0) {
+                            output.write(buffer, 0, len);
                         }
-                        out.flush();
                     }
                 }
             }
             mgfFile = outputFile;
         }
     }
-
+    
     public File getParameterFile() {
         return parameterFile;
     }
-
+    
     public File getMgfFile() {
         return mgfFile;
     }
-
+    
 }
